a8dd709aa782e6996cfaf2e1f32d8adf4ffd1741
Harshith-Reddy-Revoori <46291961+Harshith-Reddy-Revoori@users.noreply.github.com>
2022-04-26 17:18:26 +0530

    Add files via upload

---
+++ app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+/* import {AddEmployeeComponent} from './add-employee/add-employee.component';
+import {EmployeeDataComponent} from './employee-data/employee-data.component';
+import {EmployeeDetailsComponent} from './employee-details/employee-details.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { EditEmployeeComponent } from './edit-employee/edit-employee.component';*/
+const routes: Routes = [ ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
